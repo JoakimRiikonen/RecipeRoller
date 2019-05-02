f80776f7f226fbbb79ce088bec04438426011416
unknown <joakim.riiikonen@gmail.com>
2019-05-02 17:54:40 +0300

    added sample data, started view on recipes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Recipe, Ingredient, Recipe_Step
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Recipe_Step)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Recipe, Ingredient, Recipe_Step
 
 # Create your views here.
 
 def index(request):
     return render(request, 'core/index.html')
+
+def recipe(request, recipe_id):
+    print(recipe_id)
+    r = Recipe.objects.get(id=int(recipe_id))
+    print(r)
+    return render(request, 'core/recipe.html')
